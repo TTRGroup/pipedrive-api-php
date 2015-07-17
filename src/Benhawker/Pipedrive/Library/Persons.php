cd6692ed42c1226d14f3799be27f815cb057295c
atomasevic <atomasevic@gmail.com>
2015-07-17 15:40:45 +0200

    included persons:getByEmail

--- src/Benhawker/Pipedrive/Library/Persons.php
+++ src/Benhawker/Pipedrive/Library/Persons.php
@@ -50,6 +50,18 @@ class Persons
     }
 
     /**
+    +     * Returns a person / people
+    +     *
+    +     * @param  string $email pipedrive persons email
+    +     * @return array  returns details of a person
+    +     */
+    public function getByEmail($email)
+    {
+        return $this->curl->get('persons/find', array('term' => $email, 'search_by_email' => 1));
+    }
+
+
+    /**
      * Lists deals associated with a person.
      *
      * @param  array $data (id, start, limit)
