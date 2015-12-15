3d396b898b4d45dfb5a2214e54add2677286d3d0
stephanvierkant <stephan@cronius.nl>
2015-12-15 16:23:03 +0100

    added Files to constructor

--- src/Benhawker/Pipedrive/Pipedrive.php
+++ src/Benhawker/Pipedrive/Pipedrive.php
@@ -101,6 +101,12 @@ class Pipedrive
     protected $products;
 
     /**
+     * Placeholder attritube for the pipedrive files class
+     * @var Files Object
+     */
+    protected $files;
+
+    /**
      * Set up API url and load library classes
      *
      * @param string $apiKey   API key
@@ -131,6 +137,7 @@ class Pipedrive
         $this->dealFields    = new Library\DealFields($this);
         $this->organizations = new Library\Organizations($this);
         $this->products      = new Library\Products($this);
+        $this->files         = new Library\Files($this);
     }
 
     /**
